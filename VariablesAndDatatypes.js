4264235a4875a2b21e894deedaae7cf055b5e618
naveen <gownigarinaveen.k@gmail.com>
2024-09-04 01:13:07 +0530

    Variables and Datatype concepts

---
+++ VariablesAndDatatypes.js
@@ -0,0 +1,42 @@
+console.log("Hello World") 
+// Variables
+var a=10000;
+let b=20;
+const c="Constant"
+let d=true
+var e;
+var f=null
+console.log(a,b,c)
+//data types 
+console.log(typeof(a),typeof(b) ,typeof(c),typeof(d),typeof(e))
+var Array=[12,14,"String",true]
+var object={
+    name:"naveen",
+    age:24
+}
+console.log(typeof(Array), typeof(object))
+let k=2e1;// exponential 1.5* 10 power 12(1.5*10^12)
+let l=0xfa;
+console.log(l)
+console.log(k)
+if(a>10e+2) // instead of writing 1000 we can write 10e2 this used for large numbers  
+{
+    console.log("a is large")
+    console.log(10e+3)
+}
+console.log(5/0)
+let p=0x12   // 2*1+ 1*16
+console.log(p)
+let q=0x123//3*1+2*16+1*16*16
+console.log(q)
+let firstName="Naveen"
+let lastNmae="kumar"
+console.log(firstName+" "+lastNmae)
+let s=console.log("Naveen Readdy \"telusuko\" ") // escape cahrecter
+let m=0x213 //  3+16+256*2
+console.log(m)
+console.log("naveen \nkumaar\t gownigari") 
+
+let sss=null;
+console.log(typeof sss)
+console.log(typeof ("Kumar"/7))
