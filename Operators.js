84d45f2dac61648dbd7a6a2f6abc5fea61e45281
naveen <gownigarinaveen.k@gmail.com>
2024-09-04 17:50:15 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,32 @@
+//Airthmatic operator
+let a=10,b=5
+console.log(`${a} + ${b} =${a+b}`)
+console.log(`${a} - ${b} =${a-b}`)
+console.log(`${a} * ${b} =${a*b}`)
+console.log(`${a} / ${b} =${a/b}`)
+console.log(`${a} % ${b} =${a%b}`)
+let c=a++ ,d=b--
+console.log(c,d)
+let k=10
+console.log(k++,++k)
+console.log(Math.pow(2,3))
+console.log(2**3)//ES6
+
+//Realtional operator
+let m=10,n=20
+console.log(m<n)
+console.log(m>n)
+console.log(m>=n)
+console.log("Abple"> "AB")
+console.log(60> "A")
+console.log(2<"A")
+console.log(23456>"A")
+console.log(false<true)
+
+//logical operator 
+let x=10,y=15,z=20
+console.log(y>x && y<z) //it will return true if both conditions is true 
+console.log(y>x || y<z) // it will return true if one conditions is true
+console.log(x<y && "y is big")
+
+
---
+++ TypeConvertions.js
@@ -0,0 +1,16 @@
+let a=3;
+var b="naveen"
+console.log(a+" "+b)
+console.log(a-b)
+let c=2,d="1"
+console.log(c+d)
+console.log(c-d)
+let e=true
+console.log(a-true)
+let f=-1
+console.log(Boolean(f))
+let g="123.56 naveen.7"
+console.log(Number(g))
+console.log(parseFloat(g))
+console.log("Addition of "+a+" " +"and"+" "+ c+" "+"="+(a+c))//with out tempalte literals
+console.log(`Addition of ${a} and ${c} =${a+c}`) //with template literals
