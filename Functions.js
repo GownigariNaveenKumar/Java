7cd8224f6e9f503556a907fe15fbfa710c10e9e3
naveen <gownigarinaveen.k@gmail.com>
2024-09-05 22:51:19 +0530

    Functions

---
+++ Functions.js
@@ -0,0 +1,26 @@
+
+function add(a,b)
+{
+    console.log(k)
+    return a+b;
+}
+let a=add(2,3)
+var k=23
+console.log(a)
+let kk=function()
+{
+    return "Hello"
+}
+console.log(kk())
+let sum=0;
+function add1(a,b,...c)
+{
+ sum= a+b
+ for(k in c)
+ {
+   sum+= Number(c[k])
+ }
+ return sum
+}
+let kkk=add1(1,2,3,4,7)
+console.log(kkk)
