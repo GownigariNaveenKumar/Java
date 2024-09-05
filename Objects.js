124e03a841f2a2ac971e1a83faeba8e886b8e74d
naveen <gownigarinaveen.k@gmail.com>
2024-09-05 16:49:08 +0530

    Objects

---
+++ Objects.js
@@ -0,0 +1,22 @@
+let a={
+    name:'naveen',
+    'age k':24,
+    laptop:{
+        name:'lenovo',
+        ram:'16GB',
+        price:20
+    }
+
+}
+for(let k in a)
+{
+   console.log(k)
+}
+//adding properties two object
+a.amount=100;
+ a['amount1']
+ a.laptop.color="red"
+console.log(a)
+//delete the propericies from the object 
+delete a["age k"]
+console.log(a)
